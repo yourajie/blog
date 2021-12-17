c778c6f4a83433a55b8ffcde5bae03a9f83f8149
yourajie <romainhuberlant@gmail.com>
2021-12-17 08:56:50 +0100

    version ultimate

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -8,13 +8,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    /**
-     * @Route("/blog", name="blog")
+            /**
+     * @var ArticleRepository
      */
-    public function index(): Response
+    private $articleRepository;
+
+    public function __construct(articleRepository $articleRepository)
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
-        ]);
+        $this->articleRepository = $articleRepository;
+    }
+
+    /**
+     * @Route("/blog/{article}", name="article")
+     */
+    public function contact(Request $request, string $article = ""): Response
+    {
+        $name = $request->query->get('name');
+        $articleForm = new Contact();
+        $form = $this->createForm(ContactType::class, $articleForm);
+        $form->handleRequest($request)
+
+        dump($form->getViewData());
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            dump('ajout du nouvelle article')
+        }
+
+        return $this->renderForm
+        ('blog/index.html.twig',
+        [
+           'controller_name' => 'Controleur de Article',
+           'id' => $id,
+           'name' => $name,
+           'form' => $form,
+        ]
+        );
     }
 }
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,22 +2,56 @@
 
 namespace App\Controller;
 
+use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class ContactController extends AbstractController
 {
-    /**
-     * @Route("/contact/{city}", name="contactCity")
+        /**
+     * @var ContactRepository
      */
-    public function contactcity(Request $request, string $city = ""): Response
-    {
+    private $contactRepository;
 
-        return $this->render('contact/index.html.twig',[
-            'name' =>"montpellier",
-            'city' =>$city,
-        ]);
+    public function __construct(ContactRepository $contactRepository)
+    {
+        $this->contactRepository = $contactRepository;
     }
+
+
+    /**
+     * @Route("/contact/{city}", name="contact")
+     */
+    public function contact(Request $request, string $city = ""): Response
+    {
+        $name = $request->query->get('name');
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        dump($form->getViewData());
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            dump('ok en base')
+        }
+
+        return $this->renderForm
+        ('contact/index.html.twig',
+        [
+           'controller_name' => 'Controleur de Contact',
+           'city' => $city,
+           'name' => $name,
+           'form' => $form,
+        ]
+        );
+    }
+
+
 }
